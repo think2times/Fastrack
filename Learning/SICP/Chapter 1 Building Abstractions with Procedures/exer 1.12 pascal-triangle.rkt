d4227f1a68bb0c5a9cfc068fc728b1a3730a03b3
think2times <ipwding@gmail.com>
2024-08-15 23:37:06 +0800

    finish exercise 1.12 and 1.13 of SICP

--- Learning/SICP/Chapter 1 Building Abstractions with Procedures/exer 1.12 pascal-triangle.rkt
+++ Learning/SICP/Chapter 1 Building Abstractions with Procedures/exer 1.12 pascal-triangle.rkt
@@ -1,11 +1,14 @@
 #lang racket
+
 (define (pascal row col)
-  (cond ((> col row) 0)
-        ((or (= col 1) (= row col)) 1)
-        (else (+ (pascal (- row 1) (- col 1))
+  (cond ((or (< row 1) (< col 1) (< row col)) 0)     ; 排除非法参数
+        ((or (= col 1) (= row col)) 1)               ; 当该位置在两边时,值都为1
+        (else (+ (pascal (- row 1) (- col 1))        ; 其他位置的数都等于上一行“肩膀”2数之和
                  (pascal (- row 1) col)))))
 
+
 (pascal 5 1)
+(pascal 5 2)
 (pascal 5 3)
+(pascal 5 4)
 (pascal 5 5)
-      
