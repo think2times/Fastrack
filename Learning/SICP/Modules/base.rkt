6b8e1d9c6162d89820f865365a8d371447a7c1b1
pwding <xpwding@gmail.com>
2024-09-08 13:32:26 +0800

    finish exercise 2.3 of sicp

--- Learning/SICP/Modules/base.rkt
+++ Learning/SICP/Modules/base.rkt
@@ -12,6 +12,7 @@
 (provide average)
 (provide positive?)
 (provide negative?)
+(provide sqrt)
 
 
 (define pi 3.141592653589793)
@@ -51,3 +52,24 @@
 
 (define (negative? x)
   (if (< x 0) true false))
+
+(define tolerance 0.00001)
+
+(define (good-enough? guess target tolerance)
+  (< (abs (- guess target)) tolerance))
+
+; 注意:这个函数的两个参数以及返回值都是函数,所以调用的语句外还要传一个参数作为 first-guess
+(define (iterative-improve good-enough? improve-guess)
+  (lambda (first-guess)
+    (define (iter guess)
+      (if (good-enough? guess)
+          guess
+          (iter (improve-guess guess))))
+    (iter first-guess)))
+
+; Rewrite Version
+; 1.1.7 Square Roots by Newton's Method
+(define (sqrt x)
+  ((iterative-improve (lambda (guess) (good-enough? (square guess) x tolerance))
+                      (lambda (guess) (average guess (/ x guess))))
+   1.0))
