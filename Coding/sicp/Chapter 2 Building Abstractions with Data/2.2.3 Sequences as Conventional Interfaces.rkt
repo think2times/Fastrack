633c6cd7eb0281f1a5e930c2ef5778606305b00f
pwding <xpwding@gmail.com>
2024-10-09 09:04:13 +0800

    learn 'Nested Mappings' of sicp

--- Coding/sicp/Chapter 2 Building Abstractions with Data/2.2.3 Sequences as Conventional Interfaces.rkt
+++ Coding/sicp/Chapter 2 Building Abstractions with Data/2.2.3 Sequences as Conventional Interfaces.rkt
@@ -120,7 +120,51 @@
 (product-of-squares-of-odd-elements (list 1 2 3 4 5))
 
 
+; Nested Mappings
+; Find the smallest integral divisor (greater than 1)
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
 
 
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))      ; cadr 表示取第二个元素
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (flatmap
+                           (lambda (i)
+                             (map (lambda (j) (list i j))
+                                  (enumerate-interval 1 (- i 1))))
+                           (enumerate-interval 1 n)))))
+
+(prime-sum-pairs 10)
+
+(define (permutations s)
+  (if (null? s) ; empty set?
+      (list nil) ; sequence containing empty set
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
 
 
+(permutations (list 1 2 3 6))
