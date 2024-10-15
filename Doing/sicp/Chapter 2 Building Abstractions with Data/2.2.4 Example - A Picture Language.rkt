debcb694749d9a33d4c6a7014a695ab63083c653
pwding <xpwding@gmail.com>
2024-10-15 08:57:39 +0800

    finish exercise 2.46 of sicp

--- Doing/sicp/Chapter 2 Building Abstractions with Data/2.2.4 Example - A Picture Language.rkt
+++ Doing/sicp/Chapter 2 Building Abstractions with Data/2.2.4 Example - A Picture Language.rkt
@@ -74,3 +74,15 @@
 
 (paint (square-limit-2 wave 1))
 (paint (square-limit-2 wave 2))
+
+
+; Frames
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
+               (scale-vect (ycor-vect v) (edge2-frame frame))))))
+
+
+((frame-coord-map a-frame) (make-vect 0 0))
