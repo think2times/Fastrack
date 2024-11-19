7f6ba6775d3ccbfd4d6401811599f2b40413c38d
think2times <ipwding@gmail.com>
2024-11-19 10:35:14 +0800

    finish exercise 2.83 of sicp

---
+++ Doing/sicp/Chapter 2 Building Abstractions with Data/exer 2.83.rkt
@@ -0,0 +1,250 @@
+#lang racket
+
+(require racket/trace)
+(require "../Modules/base.rkt")
+
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+(define (apply-generic op . args) 
+  (define (no-method type-tags) 
+    (error "No method for these types" 
+           (list op type-tags)))
+  
+  (define (type-tags args) 
+    (map type-tag args))
+
+  ; 对每一个参数尝试进行强制类型转换
+  (define (try-coerce-to target)
+    (map (lambda (origin)
+           (if (eq? (type-tag origin) (type-tag target))
+               (lambda (x) x)   ; 如果类型一致,不进行转换
+               (let ((coercor (get-coercion (type-tag origin) (type-tag target)))) 
+                 (if coercor 
+                     (coercor origin) 
+                     origin)))) 
+         args))
+  
+  (define (iterate next) 
+    (if (null? next)
+        (no-method (type-tags args)) 
+        (let ((coerced (try-coerce-to (car next)))) 
+          (let ((proc (get op (type-tags coerced)))) 
+            (if proc 
+                (apply proc (map contents coerced)) 
+                (iterate (cdr next))))))) 
+  
+  (let ((proc (get op (type-tags args)))) 
+    (if proc 
+        (apply proc (map contents args)) 
+        (iterate args))))
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+(define (equ? x y) (apply-generic 'equ? x y))
+(define (=zero? x) (apply-generic '=zero? x))
+(define (exp x y) (apply-generic 'exp x y))
+(define (raise x) (apply-generic 'raise x))
+
+;; 数字运算包
+(define (install-scheme-number-package)
+  (put 'add '(scheme-number scheme-number) +)
+  (put 'sub '(scheme-number scheme-number) -)
+  (put 'mul '(scheme-number scheme-number) *)
+  (put 'div '(scheme-number scheme-number) /)
+  (put 'equ? '(scheme-number scheme-number) =)
+  (put '=zero? '(scheme-number) (lambda (x) (= x 0)))
+  (put 'exp '(scheme-number scheme-number)
+       ; using primitive expt
+       (lambda (x y) (attach-tag 'scheme-number (expt x y))))
+  (put 'raise '(scheme-number) (lambda (x) make-rational x 1))
+  (put 'make 'scheme-number (lambda (x) (attach-tag 'scheme-number x)))
+  'done)
+
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
+
+;; 有理数
+(define (install-rational-package)
+  ;; internal procedures
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+  (define (equ? x y)
+    ;; 先化简为最简有理数
+    (let ((simple-x (make-rat (numer x) (denom x)))
+          (simple-y (make-rat (numer y) (denom y))))
+      (and (= (numer simple-x) (numer simple-y))
+           (= (denom simple-x) (denom simple-y)))))
+
+  ;; 用十字相乘更简单
+  ;; (define (equ? x y) (= (* (numer x) (denom y)) (* (numer y) (denom x))))
+  
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'equ? '(rational rational)
+       (lambda (x y) (equ? x y)))
+  (put '=zero? '(rational) (lambda (x) (= (numer x) 0)))
+  (put 'raise '(rational) (lambda (x) (make-complex-from-real-imag x 0)))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+
+
+;; 复数
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z) (* (magnitude z) (cos (angle z))))
+  (define (imag-part z) (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-complex-package)
+  ;; imported procedures from rectangular and polar packages
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+  (define (equ? z1 z2)
+    (and (= (real-part z1) (real-part z2))
+         (= (imag-part z1) (imag-part z2))))
+  ;; interface to rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'equ? '(complex complex)
+       (lambda (z1 z2) (equ? z1 z2)))
+  (put '=zero? '(complex) (lambda (z) (= (real-part z) (imag-part z) 0)))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  'done)
+
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+
+(define (real-part z) (apply-generic 'real-part z)) 
+(define (imag-part z) (apply-generic 'imag-part z)) 
+(define (magnitude z) (apply-generic 'magnitude z)) 
+(define (angle z) (apply-generic 'angle z))
+
+(define (scheme-number->rational n)
+  (make-rational (contents n) 1))
+
+(define (rational->complex r)
+  (make-complex-from-real-imag (contents r) 0))
+
+
+(install-rectangular-package)
+(install-polar-package)
+(install-complex-package)
+(define c1 (make-complex-from-real-imag 3 4))
+(define c2 (make-complex-from-real-imag 5 12))
+
+(trace apply-generic)
+(exp c1 c2)
